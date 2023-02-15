fdd85690cee217203b1fd4c78419e4b2f5fd2ffb
MariaProcopii <mari.procopii@gmail.com>
2023-02-15 14:09:34 +0200

    refactored the structure of the project

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,6 @@ public class Main {
         System.out.println(grammar.generateWords());
         finiteAutomaton = grammar.toFiniteAutomaton();
 //        finiteAutomaton.printTransitions();
-        finiteAutomaton.wordIsValid("abababaaaa");
+        finiteAutomaton.wordIsValid("abababaaaac");
     }
 }
